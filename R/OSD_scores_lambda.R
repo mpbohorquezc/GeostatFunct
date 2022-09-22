15899ecd9f02a48c96a8f9cca71dd4fa9de3520f
Samuel-col-unal <102927261+Samuel-col-unal@users.noreply.github.com>
2022-09-22 00:42:59 -0500

    Add files via upload

--- R/OSD_scores_lambda.R
+++ R/OSD_scores_lambda.R
@@ -1,4 +1,4 @@
-.vgm_model.fn <- function(points, s0, model, method_i = "lambda", grid){
+.vgm_model.fn <- function(points, s0, model, method_i = "lambda", grid, fixed_stations){
   
   # validation ---------------------------------
   
@@ -8,11 +8,13 @@
   # Basic objects
   method <- method_i
   N <- nrow(s0)
-  k <- length(points)/2
+  points <- matrix(points,ncol=2)
+  points <- rbind(fixed_stations,points)
+  colnames(points) <- c("x","y")
+  # k <- length(points)/2
+  k <- nrow(points)
   z <- rep.int(1,k)
   L <- length(model)
-  points <- matrix(points,ncol=2)
-  colnames(points) <- c("x","y")
   df <- sp::SpatialPointsDataFrame(coords = points,
                                    data = as.data.frame(z))
   targetPoints <- sp::SpatialPoints(s0)
@@ -44,18 +46,18 @@
       
     }
     
-    tot_variance <- 0
+    accuracy <- 0
     for(l in 1:N){
       #c_vec <- rep(0,k)
       for(j in 1:L){
         #c_vec <- c_vec + gstat::variogramLine(model[[j]], dist_vector = dist_s0[,l],coveriance = T)
         c_vec <- gstat::variogramLine(model[[j]], dist_vector = dist_s0[,l],coveriance = T)
-        tot_variance <- c_vec%*%omegas[[j]]%*%t(c_vec)
+        accuracy <- c_vec%*%omegas[[j]]%*%t(c_vec)
         # Sugerencia: Ponderar por proporcion de varianza explicada.
       }
     }
     
-    return(tot_variance)
+    return(1/accuracy)
   }
   
   # ------------------ method scores
@@ -86,8 +88,9 @@
   
 }
 
-FD_optimal_design <- function(k,s0,model,method = "lambda", grid = NULL,
-                              map = NULL,nharm = NULL){
+FD_optimal_design <- function(k,s0,model,fixed_stations = NULL,
+                              method = "lambda", grid = NULL,
+                              map = NULL,nharm = NULL,plt = F){
   # validation ----------------------------------------------
   
   if (missing(k))
@@ -102,8 +105,8 @@ FD_optimal_design <- function(k,s0,model,method = "lambda", grid = NULL,
   if(is.null(grid) && is.null(map))
     stop("Missing grid and map, please give at leat one.")
   
-  
-  
+  if(!is.null(fixed_stations) && (length(intersect(class(fixed_stations),c("matrix","array","data.frame","SpatialPoints"))) == 0))
+    stop("fixed_stations must be of class matrix, array, data.frame or SpatialPoints.")
   
   if(class(k) != "numeric" || length(k)==1){
     stop("k must be a positive integer.")
@@ -112,7 +115,7 @@ FD_optimal_design <- function(k,s0,model,method = "lambda", grid = NULL,
   }
   
   if(length(intersect(class(s0),c("matrix","array","data.frame","SpatialPoints"))) == 0){
-    stop("s0 must have class matrix, array, data.frame or SpatialPoints.")
+    stop("s0 must be of class matrix, array, data.frame or SpatialPoints.")
   }
   
   if(ncol(s0)!=2)
@@ -165,7 +168,7 @@ FD_optimal_design <- function(k,s0,model,method = "lambda", grid = NULL,
   # Call optim
   stats::optim(par = c(stati0), fn = .vgm_model.fn,
                s0 = s0, model = model, method_i = method,
-               grid = grid,
+               grid = grid, fixed_stations = fixed_stations,
                method = "L-BFGS-B",
                control = list(
                  trace = 1L, 
@@ -180,6 +183,48 @@ FD_optimal_design <- function(k,s0,model,method = "lambda", grid = NULL,
   ids_tmp <- apply(as.matrix(dist_tmp),1,which.min)
   new_stati <- as.matrix(grid)[ids.tmp,]
   
-  return(new_stati)
+  
+  value <- list(new_stations = new_stati)
+  
+  # Plot -----------------------------------------
+  if (plt){
+    s0 <- as.data.frame(s0)
+    
+    if(is.null(map)){
+      ggplot2::ggplot() +
+        ggplot2::geom_point(ggplot2::aes(x = grid[,1],y = grid[,2]), colour = "gray90") + # grid
+        ggplot2::geom_point(ggplot2::aes(x = s0[,1],y = s0[,2], col = "Target Points")) + # Target points
+        ggplot2::geom_point(ggplot2::aes(x = new_stati[,1], y = new_stati[,2], col = " New Stations")) + # New stations
+        if(!is.null(fixed_stations))
+          ggplot2::geom_point(ggplot2::aes(x = fixed_stations[,1], y = fixed_stations[,2], col = "Fixed Stations")) + # Fixed stations
+        ggplot2::theme_light() +
+        ggplot2::labs(title = "Optimal Spatial Design",
+                      subtitle = paste(k,"new stations")
+                      x = "x", y = "y") + 
+        ggplot2::theme(legend.position = ggplot2::element_blank()) -> final_plot
+    } else {
+      ggplot2::ggplot() +
+        ggplot2::geom_point(ggplot2::aes(x = grid[,1],y = grid[,2]), colour = "gray90") + # grid
+        ggplot2::geom_sf(data = sf::st_as_sf(map), fill = "ffffff00", colour = "gray40") + # map
+        ggplot2::geom_point(ggplot2::aes(x = s0[,1],y = s0[,2], col = "Target Points")) + # Target points
+        ggplot2::geom_point(ggplot2::aes(x = new_stati[,1], y = new_stati[,2], col = "New Stations")) + # New stations
+        if(!is.null(fixed_stations))
+          ggplot2::geom_point(ggplot2::aes(x = fixed_stations[,1], y = fixed_stations[,2], col = "Fixed Stations")) + # Fixed stations
+        ggplot2::theme_light() +
+        ggplot2::labs(title = "Optimal Spatial Design",
+                      subtitle = paste(k,"new stations")
+                      x = "Longitude", y = "Latitude") + 
+        ggplot2::theme(legend.position = ggplot2::element_blank()) -> final_plot
+    }
+    
+    value[["plot"]] <- final_plot
+    
+  }else{
+    value[["plot"]] <- NULL
+  }
+  
+  class(value) <- "OptimalSpatialDesign"
+  return(value)
 }
+
 
