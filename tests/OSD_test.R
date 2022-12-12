37b865311cb32fc891c923fd009c4a3d1b91e6f5
Samuel-col-unal <102927261+Samuel-col-unal@users.noreply.github.com>
2022-12-12 08:45:57 -0500

    Add files via upload

--- tests/OSD_test.R
+++ tests/OSD_test.R
@@ -5,11 +5,15 @@
 source("../R/SpatFD.R")
 source("../R/OSD_scores_lambda.R")
 
-library(rgdal)
-library(gstat)
-library(sp)
+# library(rgdal)
+# library(gstat)
+# library(sp)
 
-my_map <- rgdal::readOGR("BOGOTA/Bogota.shp")
+# bogota_shp <- rgdal::readOGR("BOGOTA/Bogota.shp")
+
+# save(bogota_shp,file = "Bogota.rda")
+
+data(Bogota)
 
 vgm_model  <- gstat::vgm(psill = 5.665312,
                   model = "Exc",
@@ -22,25 +26,26 @@ vgm_model  <- gstat::vgm(psill = 5.665312,
 
 my.CRS <- sp::CRS("+init=epsg:21899") # https://epsg.io/21899
 
-my_map <- sp::spTransform(my_map,my.CRS)
-target <- sp::spsample(my_map,n = 100, type = "random") # The set of points in which we want to predict optimally.
-old_stations <- sp::spsample(my_map,n = 3, type = "random") # The set of stations that are already fixed.
+bogota_shp <- sp::spTransform(bogota_shp,my.CRS)
+target <- sp::spsample(bogota_shp,n = 100, type = "random") # The set of points in which we want to predict optimally.
+old_stations <- sp::spsample(bogota_shp,n = 3, type = "random") # The set of stations that are already fixed.
 
-ncol(target)
 
 FD_optimal_design(k = 10, s0 = target,model = vgm_model,
-               map = my_map,plt = F) -> res1
+               map = bogota_shp,plt = T) -> res1
 res1
+class(res1$new_stations)
+class(res1$fixed_stations)
 plot(res1)
 
 FD_optimal_design(k = 10, s0 = target,model = vgm_model,
-               map = my_map,plt = T,#method = "scores",
+               map = bogota_shp,plt = T,#method = "scores",
                fixed_stations = old_stations) -> res2
-
 res2
+class(res2$fixed_stations)
 plot(res2)
 
-my_grid <- sp::spsample(my_map,n = 3000, type = "regular")
+my_grid <- sp::spsample(bogota_shp,n = 3000, type = "regular")
 
 FD_optimal_design(k = 10, s0 = target,model = vgm_model,
                grid = my_grid,plt = T,nharm = 3,
@@ -48,4 +53,5 @@ FD_optimal_design(k = 10, s0 = target,model = vgm_model,
 
 res3
 plot(res3)
+
 
