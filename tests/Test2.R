44ac09877ffed22fde2f71df5be387fd06b1f669
vbejaranos <39660323+vbejaranos@users.noreply.github.com>
2023-03-06 16:19:05 +0100

    Update ex

---
+++ tests/Test2.R
@@ -0,0 +1,35 @@
+library(gstat)
+library(sp)
+library(fda)
+library(reshape)
+#library(geoR)
+library(ggplot2)
+
+
+PM10 = read.table("tests/EjemploLMCMexico2/PartMat10.txt",head=T,dec=",")
+coord = read.table("tests/EjemploLMCMexico2/Coords_DAMABog.txt",dec=",",sep="\t",header=T,row.names=1)
+
+# Create an univariate object using 2 nharm
+SFD_PM10 <- SpatFD(PM10, coords = coord, basis = "Bsplines", nbasis = 91, lambda = 0.00002, nharm = 2)
+View(SFD_PM10)
+summary(SFD_PM10)
+objects(SFD_PM10)
+
+# Create a bi-variate object using 3 nharm for the first variable and 1 nharm for the second variable
+PM10=read.table("tests/EjemploLMCMexico2/PM10.txt",head=T,dec=".")
+RAMA_Coordenadas=read.table("tests/EjemploLMCMexico2/RAMA_PM10_coordenadas.txt",head=T,row.names=1,dec=",")
+estaciones=colnames(PM10)
+CoordenadasPM10=RAMA_Coordenadas[estaciones,]
+MPM10=as.matrix(PM10,nrow=nrow(PM10),ncol=18,dimnames=c(rownames(PM10[1,4344]),colnames=colnames(PM10)))
+PM10spat=SpatFD(MPM10,CoordenadasPM10,basis="Bsplines",nbasis=21,lambda=0.00002,nharm=3)
+summary(PM10spat)
+objects(PM10spat)
+
+NO2=read.table("tests/EjemploLMCMexico2/NO2.txt",head=T,dec=".")
+estacionesNO2=read.table("tests/EjemploLMCMexico2/estacionesNO2.txt",head=T,dec=".")
+nrow(estacionesNO2)
+colnames(NO2)=rownames(estacionesNO2)
+MNO2=as.matrix(NO2,nrow=4292,ncol=13,dimnames=c(rownames(NO2),colnames=colnames(NO2)))
+PM10_MNO2_spatFD=SpatFD(MNO2, coords = estacionesNO2, basis = "Bsplines", nbasis = 27, lambda = 0.00002, nharm = 1,add=PM10spat)
+summary(PM10_MNO2_spatFD)
+objects(PM10_MNO2_spatFD)
