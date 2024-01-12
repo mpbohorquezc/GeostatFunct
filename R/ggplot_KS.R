f4e012db4aa8c83fe2cb9f1bab157a5ed2a2b4f2
vbejaranos <39660323+vbejaranos@users.noreply.github.com>
2024-01-11 19:04:08 -0500

    Update ggplot_KS.R

--- R/ggplot_KS.R
+++ R/ggplot_KS.R
@@ -101,8 +101,8 @@ ggplot_KS <- function(
       color_palette <- colorRampPalette(custom_palette)(n_colors)
       
       # Plot
-      graf[[i]] = ggplot2::ggplot(melt_s, ggplot2::aes(x= Time, y= Value, 
-                                                       col= Prediction)) +
+      graf[[i]] = ggplot2::ggplot(melt_s, ggplot2::aes_(x= ~Time, y= ~Value, 
+                                                       col= ~Prediction)) +
         ggplot2::geom_line() +
         ggplot2::scale_color_manual(values = color_palette) +
         ggplot2::labs(title = mainl[[i]] ) +
